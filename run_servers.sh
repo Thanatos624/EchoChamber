05e0cd7dd2515259787f596616ffb5f3f5e150fe
Thanatos624 <mainaksaha200603@gmail.com>
2025-07-30 01:21:45 +0530

    updated post_details.html

--- run_servers.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Start the main web server (gunicorn) on the host and port Render provides
-gunicorn blog_project.wsgi:application --bind 0.0.0.0:$PORT &
-
-# Start the chat server (daphne) on a different port in the foreground
-daphne -b 0.0.0.0 -p 10001 blog_project.asgi:application
