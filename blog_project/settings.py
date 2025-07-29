c0f82583dedd8cae83390544c84079666ad01759
Thanatos624 <mainaksaha200603@gmail.com>
2025-07-30 01:30:13 +0530

    updated settings.py and wsgi.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -142,3 +142,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
